85cb319744a2dd0d5cd65a790758d414ffb20752
hema-bonagiri <bonagirihema1491@gmail.com>
2024-03-22 17:13:01 +0530

    Task Manager

---
+++ src/app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 3000;
+
+require("./db/conn");
+app.use(express.json());
+app.use(cors()); // Enable CORS for all routes
+
+const Task = require("./models/taskinfo");
+
+app.get("/", async (req, res) => {
+  try {
+    const gettask = await Task.find({});
+    res.status(201).send(gettask);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.post("/posttask", async (req, res) => {
+  try {
+    const insertData = await Task.create(req.body);
+    console.log("Created Task Instance:", insertData);
+    res.status(201).json({ message: "Task added successfully" });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`server is listening at port number ${port}`);
+});
